5a7693ecb977eb00cbf0f8ca103c625a8099695c
SuperStarPlus1 <Ashraf@superstar-plus.co.il>
2026-08-04 02:02:27 +0300

    Add files via upload

---
+++ OneSignalSDKWorker.js
@@ -0,0 +1,6 @@
+/**
+ * OneSignal Service Worker — superstar-plus.co.il
+ * קובץ זה חייב להיות ב-root (/OneSignalSDKWorker.js)
+ * OneSignal מחפש אותו אוטומטית
+ */
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
